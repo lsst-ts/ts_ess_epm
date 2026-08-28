47729e293c7000a26452f199438d73fc8ee50557
Petr Kubánek <petr.kubanek@noirlab.edu>
2026-08-28 08:21:43 +0000

    Fix GNU license header.

--- python/lsst/ts/ess/epm/modbus/modbus_flow_meter_connector.py
+++ python/lsst/ts/ess/epm/modbus/modbus_flow_meter_connector.py
@@ -1,4 +1,4 @@
-# This file is part of ts_ess_m1m3.
+# This file is part of ts_ess_epm.
 #
 # Developed for the Vera C. Rubin Observatory Telescope and Site Systems.
 # This product includes software developed by the LSST Project
@@ -13,7 +13,7 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
